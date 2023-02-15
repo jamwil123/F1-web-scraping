a1d5400df337ca157157553a820ed551bca0f183
James <james789111@gmail.com>
2023-02-15 16:34:03 +0000

    .

--- utils/seed-data.js
+++ utils/seed-data.js
@@ -1,34 +1,28 @@
-const db = require('./db')
-
+const db = require("./db");
 
 // console.log(schema[0]["2022"])
 
-const seedData = () =>{
+const seedData = () => {
+  async function runLoop() {
+    for (let i = 1950; i <= 2022; i++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          const data = require(`../data/race-results-${i.toString()}`);
+          let year = Object.keys(data)[0];
+          db.collection("previous-races")
+            .doc(year)
+            .set(data[year])
+            .then((res) => {
+              console.log(res);
+            });
 
-    async function runLoop() {
-        for (let i = 1950; i <= 2022; i++) {
-          await new Promise(resolve => {
-            setTimeout(() => {
-                const data = require(`../data/race-results-${i.toString()}`)
-                let year = Object.keys(data)[0]
-                        db
-                                .collection('previous-races')
-                                .doc(year)
-                                .set(data[year]).then((res)=>{
-                                    console.log(res)
-                                })
-
-
-              resolve();
-            }, 0);
-          });
-        }
-      }
-      
-runLoop()
+          resolve();
+        }, 0);
+      });
     }
+  }
 
+  runLoop();
+};
 
-   
-
-seedData()
+seedData();
