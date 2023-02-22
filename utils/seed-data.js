b9c5938d2dfd9747e251f8df583b071b4f0fc571
James <james789111@gmail.com>
2023-02-22 15:03:46 +0000

    added some feature

--- utils/seed-data.js
+++ utils/seed-data.js
@@ -25,4 +25,52 @@ const seedData = () => {
   runLoop();
 };
 
-seedData();
+const seedCurrentDriversData = () => {
+  async function runLoop() {
+    for (let i = 0; i <= 19; i++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          const data = require(`../data/current-drivers/current-driver-${i}.json`);
+          let driverName = Object.keys(data[0])[0];
+          db.collection("drivers")
+            .doc(driverName)
+            .set(data[0][driverName])
+            .then((res) => {
+              console.log(`data POSTED for ${driverName}` );
+            });
+
+          resolve();
+        }, 0);
+      });
+    }
+  }
+
+  runLoop();
+};
+
+const seedCurrentTeamsData = () => {
+  async function runLoop() {
+    for (let i = 0; i <= 9; i++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          const data = require(`../data/current-teams/current-team-${i}.json`);
+          let teamName = Object.keys(data[0])[0];
+          db.collection("teams")
+            .doc(teamName)
+            .set(data[0][teamName])
+            .then((res) => {
+              console.log(`data POSTED for ${teamName}` );
+            });
+
+          resolve();
+        }, 0);
+      });
+    }
+  }
+
+  runLoop();
+};
+
+// seedCurrentDriversData()
+
+seedCurrentTeamsData()
